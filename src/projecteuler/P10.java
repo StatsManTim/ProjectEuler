7189917a3491c96c888583e9233298d7919b4722
Tim Butler <Tim.Butler7@gmail.com>
2015-10-15 22:29:20 +0100

    project7

--- src/projecteuler/P10.java
+++ src/projecteuler/P10.java
@@ -5,6 +5,12 @@
  */
 package projecteuler;
 
+import static java.lang.Math.max;
+import static java.lang.Math.sqrt;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  * @author Tim
@@ -19,4 +25,103 @@ public class P10 {
         System.out.println(sum);
     }
     
+    public static void p2(int N){
+        int temp;
+        int a = 1;
+        int b = 2;
+        int sum = 0;
+        
+        while (b < N){
+            if (b % 2 == 0)
+                sum += b;
+            temp = a;
+            a = b;
+            b += temp;
+        }
+        System.out.println(sum);
+    }
+    
+    public static void p3(double N){
+        double stopN = sqrt(N);
+        double maxFactor=0;
+        
+        
+        for (int i = 2; i <= stopN; i++){
+            //System.out.print(i + " ");
+            if (N % i == 0){
+                N /= i;
+                maxFactor = i;
+                i -= 1;
+            }
+            //System.out.println(i);
+        }
+        System.out.println(max(maxFactor,N));
+    }
+    
+    public static void p4(int a, int b){
+        String testString;       
+        int maxNum = 0;
+        int testNum;
+        for (int i = a; i < b; i++){
+            for (int j = i; j < b; j++){
+                testString = String.valueOf( i * j);
+                String reverse = new StringBuffer(testString).reverse().toString();
+                if (testString.equals(reverse)){
+                    testNum = Integer.parseInt(testString);
+                    maxNum = max(maxNum, testNum);
+                }
+            }
+        }
+        System.out.println(maxNum);
+    }
+    
+    public static void p5(int N){
+        List<Integer> primeArray = new ArrayList<>();
+        int myProd = 1;
+        for (int i=1; i<=N; i++){
+            int myNum = i;
+            for (int el: primeArray){
+                if (myNum % el == 0)
+                    myNum /= el;
+            }
+            if (myNum > 1){
+                primeArray.add(myNum); 
+                myProd *= myNum;
+            }
+        }
+    System.out.println(myProd);    
+    }
+    
+    public static void p6(int N){
+        int sumSquare = N * (2*N*N + 3*N + 1) / 6;
+        int squareSum = N*(N+1)*N*(N+1)/4;
+        int myDif = squareSum - sumSquare;
+        System.out.println(myDif);
+    }
+    
+    public static void p7(int N){
+        List<Integer> primeArray = new ArrayList<>();
+        primeArray.add(2);
+        Boolean isPrime;
+        Boolean primeFound = false;
+        int i = 2;
+        
+        while (primeFound == false){    
+            isPrime = true;
+            for (int el : primeArray) {
+                if (i % el == 0){
+                    isPrime = false;
+                    break;
+                }
+            }            
+            if (isPrime){
+                primeArray.add(i);
+            }
+            if (primeArray.size() == N){
+                System.out.println( primeArray.get(N-1) );
+                primeFound = true;
+            }
+            i++;
+        }
+    }
 }
--- src/projecteuler/ProjectEuler.java
+++ src/projecteuler/ProjectEuler.java
@@ -15,8 +15,13 @@ public class ProjectEuler {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        P10.p1(3, 5, 1000);
-        // TODO code application logic here
+        //P10.p1(3, 5, 1000);
+        //P10.p2(4000000);
+        //P10.p3(600851475143.0);
+        //P10.p4(100, 1000);
+        //P10.p5(20);
+        //P10.p6(100);
+        P10.p7(10001);
     }
     
 }
